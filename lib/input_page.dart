3043a179ed5fe79a6e265589334f6f51692ed2db
Thimira <thimiratc@gmail.com>
2020-10-06 09:34:32 +0530

    Enums and ternary operator

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,7 +5,12 @@ import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFe91e63);
+
+enum Gender {
+  male, female,
+}
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,6 +18,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,14 +35,35 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableCard(
-                      colour: activeCardColor,
-                      cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE',),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedGender = Gender.male;
+                        });
+                      },
+                      child: ReusableCard(
+                        colour: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
+                        cardChild: IconContent(
+                          icon: FontAwesomeIcons.mars,
+                          label: 'MALE',
+                        ),
+                      ),
                     ),
                   ),
                   Expanded(
-                    child: ReusableCard(colour: activeCardColor,
-                      cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE',),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedGender = Gender.female;
+                        });
+                      },
+                      child: ReusableCard(
+                        colour: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
+                        cardChild: IconContent(
+                          icon: FontAwesomeIcons.venus,
+                          label: 'FEMALE',
+                        ),
+                      ),
                     ),
                   ),
                 ],
@@ -64,7 +93,3 @@ class _InputPageState extends State<InputPage> {
         ));
   }
 }
-
-
-
-
