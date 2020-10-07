43176b948d8d3c3c061a62e1f5da58fe0ec5b671
Thimira <thimiratc@gmail.com>
2020-10-07 20:21:40 +0530

    all completed

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -7,6 +7,7 @@ import '../constants.dart';
 import 'results_page.dart';
 import '../components/bottom_button.dart';
 import '../components/round_icon_button.dart';
+import 'package:bmi_calculator/calculator_brain.dart';
 
 enum Gender {
   male,
@@ -209,8 +210,13 @@ class _InputPageState extends State<InputPage> {
                   ],
                 )),
             BottomButton(buttonTitle: 'CALCULATE', onTap: () {
+              CalculatorBrain calc = CalculatorBrain(height: height, weight: weight);
               Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ResultsPage()));
+                  MaterialPageRoute(builder: (context) => ResultsPage(
+                    bmiResult: calc.calculateBMI(),
+                    resultText:calc.getResult(),
+                    interpretation: calc.getInterpretation(),
+                  )));
             },),
           ],
         ));
--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -6,6 +6,13 @@ import '../components/reusable_card.dart';
 import '../components/bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
+
+  ResultsPage({@required this.interpretation, @required this.bmiResult, @required this.resultText});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,17 +47,17 @@ class ResultsPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    'Normal',
+                    resultText.toUpperCase(),
                     style: kResultTextStyle,
                   ),
                   Text(
-                    '18.3',
+                    bmiResult,
                     style: kBMITextStyle,
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      'Your BMI result is quite low, you should eat more',
+                      interpretation,
                       style: kBodyTextStyle,
                       textAlign: TextAlign.center,
                     ),
